aceb9ce96fa4f231eeb3d1cf48bd006389f4101b
K <kchapl@users.noreply.github.com>
2025-01-28 20:25:39 +0000

    Fix book form

---
+++ app/controllers/BookForm.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import java.time.LocalDate
+
+case class BookForm(isbn: String, startDate: LocalDate, userId: String)
+
+object BookForm {
+  def apply(isbn: String, startDate: LocalDate, userId: String): BookForm = 
+    new BookForm(isbn, startDate, userId)
+    
+  def unapply(form: BookForm): Option[(String, LocalDate, String)] = 
+    Some((form.isbn, form.startDate, form.userId))
+}
+
